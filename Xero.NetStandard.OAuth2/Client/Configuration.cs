47b65655939e671c41648e36ac5d0e8e387a6772
Jenks Guo <jenk.guo@xero.com>
2020-03-13 15:57:09 +1100

    version bumped to 0.4.2

--- Xero.NetStandard.OAuth2/Client/Configuration.cs
+++ Xero.NetStandard.OAuth2/Client/Configuration.cs
@@ -32,7 +32,7 @@ namespace Xero.NetStandard.OAuth2.Client
         /// Version of the package.
         /// </summary>
         /// <value>Version of the package.</value>
-        public const string Version = "0.4.1";
+        public const string Version = "0.4.2";
 
         /// <summary>
         /// Identifier for ISO 8601 DateTime Format
@@ -112,7 +112,7 @@ namespace Xero.NetStandard.OAuth2.Client
         [System.Diagnostics.CodeAnalysis.SuppressMessage("ReSharper", "VirtualMemberCallInConstructor")]
         public Configuration()
         {
-            UserAgent = "xero-netstandard-0.4.1";
+            UserAgent = "xero-netstandard-0.4.2";
             BasePath = "https://api.xero.com/api.xro/2.0";
             DefaultHeader = new ConcurrentDictionary<string, string>();
             ApiKey = new ConcurrentDictionary<string, string>();
@@ -345,7 +345,7 @@ namespace Xero.NetStandard.OAuth2.Client
             String report = "C# SDK (Xero.NetStandard.OAuth2) Debug Report:\n";
             report += "    OS: " + System.Runtime.InteropServices.RuntimeInformation.OSDescription + "\n";
             report += "    Version of the API: 2.0.4\n";
-            report += "    SDK Package Version: 0.4.1\n";
+            report += "    SDK Package Version: 0.4.2\n";
 
             return report;
         }
