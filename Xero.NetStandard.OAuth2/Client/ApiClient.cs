34fce910fd64973eedca15702752879b98d0e320
Robert Peter Mansion <robert.mansion@jayb.com.au>
2020-08-21 10:41:30 +0800

    Update RestSharp dependency to 106.11.4

--- Xero.NetStandard.OAuth2/Client/ApiClient.cs
+++ Xero.NetStandard.OAuth2/Client/ApiClient.cs
@@ -416,15 +416,27 @@ namespace Xero.NetStandard.OAuth2.Client
             var existingDeserializer = req.JsonSerializer as IDeserializer;
             if (existingDeserializer != null)
             {
-                client.AddHandler(existingDeserializer, "application/json", "text/json", "text/x-json", "text/javascript", "*+json", "*");
+                client.AddHandler("application/json", () => existingDeserializer);
+                client.AddHandler("text/json", () => existingDeserializer);
+                client.AddHandler("text/x-json", () => existingDeserializer);
+                client.AddHandler("text/javascript", () => existingDeserializer);
+                client.AddHandler("*+json", () => existingDeserializer);
+                client.AddHandler("*", () => existingDeserializer);
             }
             else
             {
                 var codec = new CustomJsonCodec(configuration);
-                client.AddHandler(codec, "application/json", "text/json", "text/x-json", "text/javascript", "*+json", "*");
+                client.AddHandler("application/json", () => codec);
+                client.AddHandler("text/json", () => codec);
+                client.AddHandler("text/x-json", () => codec);
+                client.AddHandler("text/javascript", () => codec);
+                client.AddHandler("*+json", () => codec);
+                client.AddHandler("*", () => codec);
             }
 
-            client.AddHandler(new XmlDeserializer(), "application/xml", "text/xml", "*+xml");
+            client.AddHandler("application/xml", () => new XmlDeserializer());
+            client.AddHandler("text/xml", () => new XmlDeserializer());
+            client.AddHandler("*+xml", () => new XmlDeserializer());
 
             client.Timeout = configuration.Timeout;
 
