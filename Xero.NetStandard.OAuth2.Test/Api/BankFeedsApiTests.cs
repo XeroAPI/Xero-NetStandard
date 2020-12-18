b1c3a2822740ab2dfe5d7ee84874cdd15baef820
Jamie Hoffmann <jamie.hoffmann@xero.com>
2020-12-18 11:18:01 +1100

    Suppress warnings on InitializeAsync and DisposeAsync

--- Xero.NetStandard.OAuth2.Test/Api/BankFeedsApiTests.cs
+++ Xero.NetStandard.OAuth2.Test/Api/BankFeedsApiTests.cs
@@ -45,6 +45,8 @@ namespace Xero.NetStandard.OAuth2.Test.Api.Bankfeeds
             this.output = output;
         }
 
+        // Methods are async but may not require it in specific cases
+        #pragma warning disable CS1998 // This async method lacks 'await' operators
         public async Task InitializeAsync()
         {
             var directory = Directory.GetCurrentDirectory();
@@ -58,6 +60,7 @@ namespace Xero.NetStandard.OAuth2.Test.Api.Bankfeeds
         {
             // Cleanup when everything is done.
         }
+        #pragma warning restore CS1998 // This async method lacks 'await' operators
 
         /// <summary>
         /// Test an instance of BankFeedsApi
