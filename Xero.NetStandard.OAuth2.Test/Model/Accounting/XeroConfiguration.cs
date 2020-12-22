1f81eb61eb08f0898d42f8f4d6cdb1a4c237bbd6
Jamie Hoffmann <jamie.hoffmann@xero.com>
2020-12-22 13:15:05 +1100

    Add API tests and empty model tests

--- Xero.NetStandard.OAuth2.Test/Model/Accounting/XeroConfiguration.cs
+++ Xero.NetStandard.OAuth2.Test/Model/Accounting/XeroConfiguration.cs
@@ -7,5 +7,6 @@ namespace Xero.NetStandard.OAuth2.Test.Model.Accounting
     {
         public string AccountingBaseUrl { get; set; }
         public string BankfeedsBaseUrl { get; set; }
+        public string PayrollAuBaseUrl { get; set; }
     }
 }
