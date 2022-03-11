e24d38722544b3b91ffaff58c5b63e91e09da1fe
Rett Behrens <rett@rettbehrens.codes>
2022-03-11 13:52:28 -0700

    generates xero-netstandard-3.23.0 from OAS 2.19.3

--- Xero.NetStandard.OAuth2/Client/Configuration.cs
+++ Xero.NetStandard.OAuth2/Client/Configuration.cs
@@ -30,7 +30,7 @@ namespace Xero.NetStandard.OAuth2.Client
         /// Version of the package.
         /// </summary>
         /// <value>Version of the package.</value>
-        public const string Version = "3.22.2";
+        public const string Version = "3.23.0";
 
         /// <summary>
         /// Identifier for ISO 8601 DateTime Format
@@ -102,7 +102,7 @@ namespace Xero.NetStandard.OAuth2.Client
         [System.Diagnostics.CodeAnalysis.SuppressMessage("ReSharper", "VirtualMemberCallInConstructor")]
         public Configuration()
         {
-            UserAgent = "xero-netstandard-3.22.2";
+            UserAgent = "xero-netstandard-3.23.0";
             BasePath = "https://api.xero.com/api.xro/2.0";
             DefaultHeader = new ConcurrentDictionary<string, string>();
             ApiKey = new ConcurrentDictionary<string, string>();
@@ -335,7 +335,7 @@ namespace Xero.NetStandard.OAuth2.Client
             String report = "C# SDK (Xero.NetStandard.OAuth2) Debug Report:\n";
             report += "    OS: " + System.Runtime.InteropServices.RuntimeInformation.OSDescription + "\n";
             report += "    Version of the API: 2.19.3\n";
-            report += "    SDK Package Version: 3.22.2\n";
+            report += "    SDK Package Version: 3.23.0\n";
 
             return report;
         }
