6cec2f9302b6c552c2d22cd8e3c37612efaf72c3
Anu Thomas <anu.thomas@xero.com>
2024-01-25 20:14:39 +0530

    Build from OpenAPI V2.40.3 updates the SDK version to 3.34.5

--- Xero.NetStandard.OAuth2/Client/Configuration.cs
+++ Xero.NetStandard.OAuth2/Client/Configuration.cs
@@ -31,7 +31,7 @@ namespace Xero.NetStandard.OAuth2.Client
         /// Version of the package.
         /// </summary>
         /// <value>Version of the package.</value>
-        public const string Version = "3.34.4";
+        public const string Version = "3.34.5";
 
         /// <summary>
         /// Identifier for ISO 8601 DateTime Format
@@ -103,7 +103,7 @@ namespace Xero.NetStandard.OAuth2.Client
         [System.Diagnostics.CodeAnalysis.SuppressMessage("ReSharper", "VirtualMemberCallInConstructor")]
         public Configuration()
         {
-            UserAgent = "xero-netstandard-3.34.4";
+            UserAgent = "xero-netstandard-3.34.5";
             BasePath = "https://api.xero.com/api.xro/2.0";
             DefaultHeader = new ConcurrentDictionary<string, string>();
             ApiKey = new ConcurrentDictionary<string, string>();
@@ -342,7 +342,7 @@ namespace Xero.NetStandard.OAuth2.Client
             String report = "C# SDK (Xero.NetStandard.OAuth2) Debug Report:\n";
             report += "    OS: " + System.Runtime.InteropServices.RuntimeInformation.OSDescription + "\n";
             report += "    Version of the API: 2.40.3\n";
-            report += "    SDK Package Version: 3.34.4\n";
+            report += "    SDK Package Version: 3.34.5\n";
 
             return report;
         }
