c90d7298875ca434ee6df92845b31417f6298638
yosefmah <yosef.mahmood@gmail.com>
2024-05-23 17:11:51 +0100

    Update RestSharp to latest version

--- Xero.NetStandard.OAuth2/Client/ApiClient.cs
+++ Xero.NetStandard.OAuth2/Client/ApiClient.cs
@@ -344,7 +344,7 @@ namespace Xero.NetStandard.OAuth2.Client
             
             RestRequest request = new RestRequest(path, Method(method))
             {
-                Timeout = configuration.Timeout
+                Timeout = TimeSpan.FromMilliseconds(configuration.Timeout)
             };
 
             if (options.PathParameters != null)
@@ -501,7 +501,7 @@ namespace Xero.NetStandard.OAuth2.Client
         {
             RestClientOptions clientOptions = new RestClientOptions(_baseUrl)
             {
-                MaxTimeout = configuration.Timeout
+                Timeout = TimeSpan.FromMilliseconds(configuration.Timeout)
             };
             if (configuration.UserAgent != null)
             {
