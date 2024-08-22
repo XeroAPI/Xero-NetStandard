a92a674ded77aedbe9213d1eea86440920acc07f
sangeet-joy_xero <sangeet.joy@xero.com>
2024-08-22 14:59:16 +0530

    patch version change

--- Xero.NetStandard.OAuth2/Client/Configuration.cs
+++ Xero.NetStandard.OAuth2/Client/Configuration.cs
@@ -32,7 +32,7 @@ namespace Xero.NetStandard.OAuth2.Client
         /// Version of the package.
         /// </summary>
         /// <value>Version of the package.</value>
-        public const string Version = "8.1.0";
+        public const string Version = "8.1.1";
 
         /// <summary>
         /// Identifier for ISO 8601 DateTime Format
@@ -104,7 +104,7 @@ namespace Xero.NetStandard.OAuth2.Client
         [System.Diagnostics.CodeAnalysis.SuppressMessage("ReSharper", "VirtualMemberCallInConstructor")]
         public Configuration()
         {
-            UserAgent = "xero-netstandard-8.1.0";
+            UserAgent = "xero-netstandard-8.1.1";
             BasePath = "https://api.xero.com/api.xro/2.0";
             DefaultHeader = new ConcurrentDictionary<string, string>();
             ApiKey = new ConcurrentDictionary<string, string>();
@@ -343,7 +343,7 @@ namespace Xero.NetStandard.OAuth2.Client
             String report = "C# SDK (Xero.NetStandard.OAuth2) Debug Report:\n";
             report += "    OS: " + System.Runtime.InteropServices.RuntimeInformation.OSDescription + "\n";
             report += "    Version of the API: 6.1.1\n";
-            report += "    SDK Package Version: 8.1.0\n";
+            report += "    SDK Package Version: 8.1.1\n";
 
             return report;
         }
