b751fe042e1036d1a4097d56faeebc098fd39ac5
vigneshk-tw <vignesh.kennadi@xero.com>
2024-10-09 17:58:22 +0530

    updated format script

--- scripts/format.sh
+++ scripts/format.sh
@@ -1,6 +1,5 @@
 
 SCRIPT_DIR=$(dirname "$0")
 PROJECT_ROOT=$(realpath "${SCRIPT_DIR}/..")
-cd "${PROJECT_ROOT}" && dotnet format
-cd ..
-chmod -R 755 Xero-NetStandard
+cd "${PROJECT_ROOT}"
+dotnet format
